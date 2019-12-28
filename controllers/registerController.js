fcf53f966430bf9f438cde85226f412c736caeef
Fikri Ramadhan <fikri@inarts.co.id>
2019-12-28 10:13:50 +0700

    validasi register

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,41 +1,36 @@
 const Register = require("./../models/register");
-
+const {
+	validationResult
+} = require('express-validator/check');
 exports.store = (req, res) => {
+	const errors = validationResult(req)
+	if (!errors.isEmpty()) {
+		return res.status(422).json({
+			errors: errors.array()
+		});
+	}
 	const register = new Register(req.body);
-	console.log(register);
+	res.send('success')
 
-	// register.save((err, result) => {
-	// 	if (err) {
-	// 		return res.status(400).json({
-	// 			error: err
-	// 		})
-	// 	}
-	// 	res.status(200).json({
-	// 		message: result
-	// 	})
-	// })
+	register.save((err, result) => {
+		if (err) {
+			return res.status(400).json({
+				error: err
+			})
+		}
+		res.status(200).json({
+			message: result
+		})
+	})
 };
 
-// exports.getUsername = async (username) => {
-// 	const query = {
-// 		username: username
-// 	}
-// 	try {
-// 		return await Register.find(query);
-// 	} catch (err) {
-// 		throw (err);
-// 	}
-// };
-
-
-exports.getUsername = (username) => {
+exports.getUsername = async (username) => {
 	const query = {
 		username: username
 	}
-	return Register.find(query, (err, obj) => {
-		var data;
-		if (obj.length > 1) {
-			return data = obj;
-		}
-	})
+	try {
+		return await Register.find(query);
+	} catch (err) {
+		throw (err);
+	}
 };
--- routes/register.js
+++ routes/register.js
@@ -1,10 +1,31 @@
 const express = require("express")
 const router = express.Router()
 const registerController = require("./../controllers/registerController")
+const Register = require("./../models/register")
 const {
-    createUser
-} = require("./../validators/register")
+    check,
+} = require('express-validator/check');
 
-router.post("/", createUser, registerController.store)
+
+// router.post('/', [createUser], registerController.store);
+
+router.post('/', [
+    check('username').custom(value => {
+        return Register.findOne({
+            'username': value
+        }).then(user => {
+            if (user) {
+                return Promise.reject('xx');
+            }
+        });
+    }),
+    check('password').custom((value, {
+        req
+    }) => {
+        if (value !== req.body.passwordConfirmation) {
+            throw new Error('Password confirmation is incorrect');
+        }
+    })
+], registerController.store);
 
 module.exports = router
